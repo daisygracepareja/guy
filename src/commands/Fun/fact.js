f07516c851646beaf203e77c59a321c0cd1ae37c
daisygracepareja <daisygrace.pareja@deped.gov.ph>
2026-05-24 17:33:29 +0800

    Update fact.js

--- src/commands/Fun/fact.js
+++ src/commands/Fun/fact.js
@@ -6,6 +6,7 @@ import { handleInteractionError, TitanBotError, ErrorTypes } from '../../utils/e
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 const facts = [
   "A day on Venus is longer than a year on Venus.",
+  "A guy named Cornelios made this bot"
   "The shortest war in history was between Britain and Zanzibar on August 27, 1896. It lasted 38 to 45 minutes.",
   "The word 'Strengths' is the longest word in the English language with only one vowel.",
   "Octopuses have three hearts and blue blood.",
@@ -23,7 +24,7 @@ export default {
     try {
       const randomFact = facts[Math.floor(Math.random() * facts.length)];
 
-      const embed = successEmbed("🧠 Did You Know?", `💡 **${randomFact}**`);
+      const embed = successEmbed("did You Know?", `💡 **${randomFact}**`);
 
       await InteractionHelper.safeReply(interaction, { embeds: [embed] });
       logger.debug(`Fact command executed by user ${interaction.user.id} in guild ${interaction.guildId}`);
